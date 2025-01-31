438fa0213f421ce520d5e80325414a50920f8ecd
dongguJeong <zzzjdg@naver.com>
2025-01-31 12:02:58 +0900

    fix : 프리패칭 삭제

--- src/hooks/useDetailNeighbor.ts
+++ src/hooks/useDetailNeighbor.ts
@@ -1,33 +1,19 @@
 import { useMemo } from 'react';
 import { useTypedSelector } from './redux';
-import { QueryClient, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { CategoryCode } from '../models/DetailNeighbor.model';
-import {
-    getNeighborFromCategory,
-    prefetchNeighborbyCategory,
-} from '../utils/categorySearch';
-import { categories } from '../utils/constants';
+import { getNeighborFromCategory } from '../utils/categorySearch';
 
 interface useDetailNeighborProps {
     categoryId: CategoryCode;
 }
 
 export const useDetailNeighbor = ({ categoryId }: useDetailNeighborProps) => {
-    const queryClient = new QueryClient();
     const { detailInfo } = useTypedSelector((state) => state.detail);
     const position = useMemo(
         () => ({ lat: detailInfo!.y, lng: detailInfo!.x }),
         [detailInfo],
     );
-
-    categories.forEach((category) => {
-        prefetchNeighborbyCategory({
-            queryClient,
-            id: category.id,
-            lat: position.lat,
-            lng: position.lng,
-        });
-    });
 
     const { data: neighbors, isLoading } = useQuery({
         queryKey: ['category', position.lat, position.lng, categoryId],
--- src/utils/categorySearch.ts
+++ src/utils/categorySearch.ts
@@ -28,22 +28,3 @@ export const getNeighborFromCategory = async (
         );
     });
 };
-
-interface prefetchNeighborbyCategoryProps {
-    queryClient: QueryClient;
-    id: CategoryCode;
-    lat: number;
-    lng: number;
-}
-
-export const prefetchNeighborbyCategory = async ({
-    queryClient,
-    id,
-    lat,
-    lng,
-}: prefetchNeighborbyCategoryProps) => {
-    await queryClient.prefetchQuery({
-        queryKey: ['category', lat, lng, id],
-        queryFn: () => getNeighborFromCategory(id, lat, lng),
-    });
-};
